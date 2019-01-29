ef825d25dcaf49fc091903e4be09b6be0abd61dc
Dequan Johnson <johnsondequan900@gmail.com>
2019-01-28 19:32:26 -0500

    Added test that checks if new feed is loaded when new feed list item is selected Also tests if Feed list is empty Also test if Feed list item is clicked

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,7 +8,7 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-$(function() {
+$(function () {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
@@ -50,26 +50,26 @@ $(function() {
 
     /* New test suite named "The menu" */
     describe('The menu', () => {
+        let feedListMenu = $('body');
         /* This is a function that ensures the menu element is
          * hidden by default.
          */
         it('menu should be hidden by default', () => {
-            expect( $('body').hasClass('menu-hidden') ).toBe(true);
+            expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-         /* This is a function that ensures the menu changes
-          * visibility when the menu icon is clicked. This test has
-          * two expectations: does the menu display when clicked and
-          * does it hide when clicked again.
-          */
-         it('should be visible when clicked and hidden when clicked', () => {
-             let sideMenu = $('body'),
-             menuIcon = $('.menu-icon-link');
-             menuIcon.click();
-             expect( $(sideMenu).hasClass('menu-hidden') ).toBe(false);
-             menuIcon.click();
-             expect( $(sideMenu).hasClass('menu-hidden') ).toBe(true);
-         });
+        /* This is a function that ensures the menu changes
+         * visibility when the menu icon is clicked. This test has
+         * two expectations: does the menu display when clicked and
+         * does it hide when clicked again.
+         */
+        it('should be visible when clicked and hidden when clicked', () => {
+            let menuIcon = $('.menu-icon-link');
+            menuIcon.click();
+            expect(feedListMenu.hasClass('menu-hidden')).toBe(false);
+            menuIcon.click();
+            expect(feedListMenu.hasClass('menu-hidden')).toBe(true);
+        });
     });
 
     /* New test suite named "Initial Entries" */
@@ -78,20 +78,62 @@ $(function() {
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          */
-        beforeEach(function (done) {
+        beforeEach((done) => {
             loadFeed(0, done);
         });
-        it('entries should be more than 0', (done) => {
+        it('Entry should be more than 0', (done) => {
             let entriesLen = $(".feed .entry").length;
             expect(entriesLen).toBeGreaterThan(0);
             done();
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+    /* New test suite named "New Feed Selection" */
+    describe('New Feed Selection', () => {
+        /* This is a asynchronous function that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
          */
+        let feedListMenu = $('body'),
+            feedList = $('.feed-list'),
+            feeds = feedList.find('li'), // Find out how many feeds exist
+            currentFeeds, // Current list of feeds being displayed
+            arrayOfFirstFeeds = [],
+            arrayOfSecondsFeeds = [];
+
+        beforeEach((done) => {
+            loadFeed(0, done); // Loads the starting feed
+            // setTimeout is needed because sometimes the other functions occur
+            // before the first loadFeed function is complete.
+            setTimeout(() => {
+                currentFeeds = $('.feed .entry-link');
+                // Converts the link elements from the first feed into an array
+                Array.from(currentFeeds).forEach(feed => {
+                    arrayOfFirstFeeds.push(feed);
+                });
+                loadFeed(1, done); // Loads the second feed
+            }, 1000);
+        });
+
+        it('should load new feed when new feed list item is selected', () => {
+            currentFeeds = $('.feed .entry-link');
+            // Converts the link elements from the second feed into an array
+            Array.from(currentFeeds).forEach(feed => {
+                arrayOfSecondsFeeds.push(feed);
+            });
+
+            // Feed list should not be empty
+            expect(feeds.length).not.toBe(0);
+
+            // Feed list item clicked
+            feedList.click();
+            expect(feedListMenu.hasClass('menu-hidden')).toBe(true);
+
+            // For loop check through the complete array of first feeds
+            // Compare  each link to the second array links in the exact positions
+            // If they do not match then the 2 feeds are indeed different
+            for (let index = 0; index < arrayOfFirstFeeds.length; index++) {
+                expect(arrayOfFirstFeeds[index] === arrayOfSecondsFeeds[index]).toBe(false);
+            }
+        });
+    });
 }());
